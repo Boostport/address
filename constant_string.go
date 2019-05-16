95209c6a21fa793bf7e07ff9bddb937f1bd7f7e4
Francis Chuang <francis.chuang@boostport.com>
2019-05-17 09:12:02 +1000

    Use docker-compose for live-reload and go generate, pin stringer to go.mod and regenerate strings

--- constant_string.go
+++ constant_string.go
@@ -4,6 +4,21 @@ package address
 
 import "strconv"
 
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[Country-1]
+	_ = x[Name-2]
+	_ = x[Organization-3]
+	_ = x[StreetAddress-4]
+	_ = x[DependentLocality-5]
+	_ = x[Locality-6]
+	_ = x[AdministrativeArea-7]
+	_ = x[PostCode-8]
+	_ = x[SortingCode-9]
+}
+
 const _Field_name = "CountryNameOrganizationStreetAddressDependentLocalityLocalityAdministrativeAreaPostCodeSortingCode"
 
 var _Field_index = [...]uint8{0, 7, 11, 23, 36, 53, 61, 79, 87, 98}
@@ -14,6 +29,33 @@ func (i Field) String() string {
 		return "Field(" + strconv.FormatInt(int64(i+1), 10) + ")"
 	}
 	return _Field_name[_Field_index[i]:_Field_index[i+1]]
+}
+func _() {
+	// An "invalid array index" compiler error signifies that the constant values have changed.
+	// Re-run the stringer command to generate them again.
+	var x [1]struct{}
+	_ = x[Area-1]
+	_ = x[City-2]
+	_ = x[County-3]
+	_ = x[Department-4]
+	_ = x[District-5]
+	_ = x[DoSi-6]
+	_ = x[Eircode-7]
+	_ = x[Emirate-8]
+	_ = x[Island-9]
+	_ = x[Neighborhood-10]
+	_ = x[Oblast-11]
+	_ = x[PINCode-12]
+	_ = x[Parish-13]
+	_ = x[PostTown-14]
+	_ = x[PostalCode-15]
+	_ = x[Prefecture-16]
+	_ = x[Province-17]
+	_ = x[State-18]
+	_ = x[Suburb-19]
+	_ = x[Townland-20]
+	_ = x[VillageTownship-21]
+	_ = x[ZipCode-22]
 }
 
 const _FieldName_name = "AreaCityCountyDepartmentDistrictDoSiEircodeEmirateIslandNeighborhoodOblastPINCodeParishPostTownPostalCodePrefectureProvinceStateSuburbTownlandVillageTownshipZipCode"
--- go.mod
+++ go.mod
@@ -1,6 +1,9 @@
 module github.com/Boostport/address
 
+go 1.12
+
 require (
 	github.com/hashicorp/go-multierror v1.0.0
-	golang.org/x/text v0.3.0
+	golang.org/x/text v0.3.2
+	golang.org/x/tools v0.0.0-20190516015132-d1a3278ee749 // indirect
 )
