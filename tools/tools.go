f88736cd7be26150d1050dac6976a7c435d2125e
Nate Brennand <nbrennand@bolt.com>
2022-06-19 17:26:06 -0700

    [tooling deps] use sub package rather than build tags

---
+++ tools/tools.go
@@ -0,0 +1,13 @@
+//go:build tools
+
+package tools
+
+import (
+	// Import all required tools so that they can be version controlled via go.mod & go.sum.
+	// These imports are tracked a sub package so as to not impact the dependencies of clients of
+	// the library.
+	// This should be migrated directly to go.mod when the following is complete:
+	// https://github.com/golang/go/issues/48429
+	_ "github.com/unchartedsoftware/witch"
+	_ "golang.org/x/tools/cmd/stringer"
+)
